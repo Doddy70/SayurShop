41c67b964c50ff403e3a327ac22334fca0d7846b
Doddy70 <69391367+Doddy70@users.noreply.github.com>
2025-04-01 00:17:38 +0700

    SayurShop React
    Commit 2

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import {
 } from "firebase/messaging";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyA3lFknjE3vp8rVZsg5ab83CxGLb6BCHz4",
+  authDomain: "pitara-sejasa.firebaseapp.com",
+  projectId: "pitara-sejasa",
+  storageBucket: "pitara-sejasa.appspot.com",
+  messagingSenderId: "81515021398",
+  appId: "1:81515021398:web:3783d2fa2edcfef99167f7",
+  measurementId: "G-ED7LCR20KJ",
 };
 const firebaseApp = !getApps().length
   ? initializeApp(firebaseConfig)
--- src/utils/staticCredential.js
+++ src/utils/staticCredential.js
@@ -1,5 +1,5 @@
 export const google_client_id =
-  "";
+  "81515021398-6pvisaim9b5ij3e3t5bl3srh7lutc3po.apps.googleusercontent.com";
 export const fb_app_id = "";
 export const appleLoginCredential = {
   serviceId: "",
